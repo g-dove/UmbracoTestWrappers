3219ca479785a856dfe068c7980deeae98a30599
jake williamson <jake.williamson@gibedigital.com>
2016-06-09 15:43:23 +0100

    added a wrapper for the umbraco helper, added a wrapper for 'GetPreValueAsString'

---
+++ UmbracoWrappers/IUmbracoHelperWrapper.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Web;
+
+namespace Gibe.UmbracoWrappers
+{
+	public interface IUmbracoHelperWrapper
+	{
+		UmbracoHelper UmbracoHelper();
+
+		string GetPreValueAsString(int id);
+	}
+}
