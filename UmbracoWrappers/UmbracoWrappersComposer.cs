3efd493c920331bc81e65ab6730d40977d8cd472
Steve Temple <steve@gibedigital.com>
2019-04-10 15:30:00 +0100

    Add v8 composer

---
+++ UmbracoWrappers/UmbracoWrappersComposer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Composing;
+
+namespace Gibe.UmbracoWrappers
+{
+	public class UmbracoWrappersComposer : IComposer
+	{
+		public void Compose(Composition composition)
+		{
+			composition.RegisterFor<IUmbracoWrapper, DefaultUmbracoWrapper>();
+		}
+	}
+}
